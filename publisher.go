e68c361f20d7cee0f829338e337bc2f9cff7eee2
trueifnotfalse <trueifnotfalse@gmail.com>
2024-09-06 11:14:40 +0700

    message timestamp

--- publisher.go
+++ publisher.go
@@ -1,114 +1,123 @@
 package rabbitmq
 
 import (
-	"encoding/json"
-	"github.com/streadway/amqp"
+    "encoding/json"
+    "github.com/streadway/amqp"
+    "time"
 )
 
 type Publisher struct {
-	connector    *Connector
-	exchangeName string
-	routingKey   string
-	deliveryMode uint8
-	contentType  string
-	body         []byte
-	obj          interface{}
-	expiration   string
+    connector    *Connector
+    exchangeName string
+    routingKey   string
+    deliveryMode uint8
+    contentType  string
+    body         []byte
+    obj          interface{}
+    expiration   string
+    timestamp    time.Time
 }
 
 func newPublisher(connector *Connector) *Publisher {
-	return &Publisher{
-		connector:    connector,
-		deliveryMode: amqp.Persistent,
-		contentType:  "text/plain",
-	}
+    return &Publisher{
+        connector:    connector,
+        deliveryMode: amqp.Persistent,
+        contentType:  "text/plain",
+    }
 }
 
 func (p *Publisher) ToExchange(name string) *Publisher {
-	p.exchangeName = name
+    p.exchangeName = name
 
-	return p
+    return p
 }
 
 func (p *Publisher) ToQueue(name string) *Publisher {
-	p.routingKey = name
+    p.routingKey = name
 
-	return p
+    return p
 }
 
 func (p *Publisher) AsPersistent() *Publisher {
-	p.deliveryMode = amqp.Persistent
+    p.deliveryMode = amqp.Persistent
 
-	return p
+    return p
 }
 
 func (p *Publisher) AsTransient() *Publisher {
-	p.deliveryMode = amqp.Transient
+    p.deliveryMode = amqp.Transient
 
-	return p
+    return p
 }
 
 func (p *Publisher) WithExpiration(expiration string) *Publisher {
-	p.expiration = expiration
+    p.expiration = expiration
 
-	return p
+    return p
+}
+
+func (p *Publisher) WithTimestamp(timestamp time.Time) *Publisher {
+    p.timestamp = timestamp
+
+    return p
 }
 
 func (p *Publisher) WithContentType(contentType string) *Publisher {
-	p.contentType = contentType
+    p.contentType = contentType
 
-	return p
+    return p
 }
 
 func (p *Publisher) WithData(data []byte) *Publisher {
-	p.body = data
+    p.body = data
 
-	return p
+    return p
 }
 
 func (p *Publisher) WithStruct(obj interface{}) *Publisher {
-	p.obj = obj
+    p.obj = obj
 
-	return p
+    return p
 }
 
 func (p *Publisher) Do() error {
-	channel, err := p.connector.GetChannel()
-	if nil != err {
-		return err
-	}
-	defer channel.Close()
-	body, err := p.getBody()
-	if nil != err {
-		return err
-	}
+    channel, err := p.connector.GetChannel()
+    if nil != err {
+        return err
+    }
+    defer channel.Close()
+    body, err := p.getBody()
+    if nil != err {
+        return err
+    }
 
-	return channel.Publish(
-		p.exchangeName,
-		p.routingKey,
-		false,
-		false,
-		amqp.Publishing{
-			ContentType:  p.contentType,
-			Body:         body,
-			DeliveryMode: p.deliveryMode,
-			Expiration:   p.expiration,
-		})
+    return channel.Publish(
+        p.exchangeName,
+        p.routingKey,
+        false,
+        false,
+        amqp.Publishing{
+            ContentType:  p.contentType,
+            Body:         body,
+            DeliveryMode: p.deliveryMode,
+            Expiration:   p.expiration,
+            Timestamp:    p.timestamp,
+        })
 }
 
 func (p *Publisher) getBody() ([]byte, error) {
-	if nil != p.obj {
-		return p.unmarshalStruct()
-	}
+    if nil != p.obj {
+        return p.unmarshalStruct()
+    }
 
-	return p.body, nil
+    return p.body, nil
 }
 
 func (p *Publisher) unmarshalStruct() ([]byte, error) {
-	objWithMarshalJSON, ok := p.obj.(json.Marshaler)
-	if ok {
-		return objWithMarshalJSON.MarshalJSON()
-	}
+    objWithMarshalJSON, ok := p.obj.(json.Marshaler)
+    if ok {
+        return objWithMarshalJSON.MarshalJSON()
+    }
 
-	return json.Marshal(p.obj)
+    return json.Marshal(p.obj)
 }
